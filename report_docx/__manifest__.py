a1bb12fd34708af8a1af6ea4c9531f9621827a45
保定-粉刷匠 <openerp@126.com>
2024-09-29 09:48:29 +0800

    更改report_docx模块版本格式来符合Odoo18要求

--- report_docx/__manifest__.py
+++ report_docx/__manifest__.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Report Docx",
-    "version": '18.0.1.0',
+    "version": '18.1.0.0',
     "category": "Report",
     "website": "",
     "author": "Elico Corp, Odoo Community Association (OCA), "
